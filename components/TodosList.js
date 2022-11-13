76889d3466b6700191942c4359750b362074419b
JiaeK <jiaekm@gmail.com>
2022-11-13 20:27:22 +0100

    initial commit (index.js prob)

---
+++ components/TodosList.js
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+
+export default function TodosList() {
+    const TodosList = props => {
+        return (
+          <ul>
+            {props.todos.map(todo => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                handleChangeProps={props.handleChangeProps}
+                deleteTodoProps={props.deleteTodoProps}
+                setUpdate={props.setUpdate}
+              />
+            ))}
+          </ul>
+        )
+      }
+}
